f7c5ea6cc11359583b8e0bdabc5fee559786f9f5
Rahul Raj <rr22mcf1r39@student.nitw.ac.in>
2024-10-11 20:17:03 +0530

    Semester memo created

---
+++ result-memo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+import ResultMemo from "./ResultMemoComponent";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ResultMemo />
+    </div>
+  );
+};
+
+export default App;
